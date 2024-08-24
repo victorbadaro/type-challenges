33bef5c1bf3c0d7bbad14559206d58e6af6fd533
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-08-24 16:51:18 -0300

    feat: add IsAny and NotAny type helpers

--- src/helpers/type-utils.ts
+++ src/helpers/type-utils.ts
@@ -3,3 +3,5 @@ export type Equal<X, Y> =
 	(<T>() => T extends X ? 1 : 2) extends
 	(<T>() => T extends Y ? 1 : 2) ? true : false;
 export type NotEqual<X, Y> = true extends Equal<X, Y> ? false : true;
+export type IsAny<T> = 0 extends (1 & T) ? true : false;
+export type NotAny<T> = true extends IsAny<T> ? false : true;
